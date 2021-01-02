23bdcfc4f673a6e4d235f793c764e2eefc0c00ed
Lucien <lucienzhangzl@outlook.com>
2021-01-02 15:50:19 +0900

    practice problems

--- homework/Chapter02/Example_macro_types/macro_types.c
+++ homework/Chapter02/Example_macro_types/macro_types.c
@@ -1,10 +1,12 @@
 #include <inttypes.h>
+#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
     int32_t x = INT32_MIN;
     uint64_t y = UINT64_MAX;
-    printf("x = %" PRId32 ", y = %" PRIu64 "\n", x, y);
+    int z = INT_MAX;
+    printf("x = %" PRId32 ", y = %" PRIu64 ", z = %d\n", x, y, z);
     return 0;
 }
